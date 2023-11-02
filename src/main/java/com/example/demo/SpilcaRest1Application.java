5c42bbf5ab0c66a746b8de8700bf27cb18bbe693
Pedro Fanelli <pedrofanelli1@gmail.com>
2023-11-02 19:09:34 -0300

    INIT

---
+++ src/main/java/com/example/demo/SpilcaRest1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpilcaRest1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpilcaRest1Application.class, args);
+	}
+
+}
