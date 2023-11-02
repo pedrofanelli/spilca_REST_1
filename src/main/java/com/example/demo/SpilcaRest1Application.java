9e0fd10b1cca2e348c3e2a910582967c017789eb
Pedro Fanelli <pedrofanelli1@gmail.com>
2023-11-02 19:20:00 -0300

    building endpoint

--- src/main/java/com/example/demo/SpilcaRest1Application.java
+++ src/main/java/com/example/demo/SpilcaRest1Application.java
@@ -2,8 +2,20 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+/*
+ * We will use 2 tools: Spring Cloud OpenFeign and WebClient(Web Flux)
+ * 
+ * This app represents the "payment service" which is the REST endpoint
+ * 
+ * An external API where I collect info
+ */
+
+
 
 @SpringBootApplication
+@ComponentScan(basePackages= {"controllers"})
 public class SpilcaRest1Application {
 
 	public static void main(String[] args) {
